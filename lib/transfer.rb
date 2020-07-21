3a5a781e367e87b4b73e89a221bc08c55708ba0c
Carter Parrett <cparrett94@hotmail.com>
2020-07-21 23:15:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
    self.sender.valid? && self.receiver.valid?
  end
  def execute_transaction
-   if self.sender.balance > self.amount && self.sender.valid? == true
+   if self.sender.balance > self.amount && self.sender.valid? == true && self.status = "pending"
    self.sender.balance -= amount
    self.receiver.balance += amount
    self.status = "complete"
