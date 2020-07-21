bd7014088071c91631c84b978cb23a9885ad1853
Carter Parrett <cparrett94@hotmail.com>
2020-07-21 23:27:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,9 @@ class Transfer
       "Transaction rejected. Please check your account balance."
   end
  end
-   
+   def reverse_transfer
+     self.execute_transaction.reverse
+   end
  
  
 end
